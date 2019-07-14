dee04c06c0025e2ecb99af74ef542c8c22f2b68e
yangchuanhu <ych_1024@163.com>
2019-07-14 22:34:10 +0800

    add gomodules version demo

---
+++ ch25-gomodules-version/go.mod
@@ -0,0 +1,10 @@
+module ch25-gomodules-version
+
+require github.com/cnych/stardust v1.0.1
+
+require (
+	github.com/cnych/stardust/v2 v2.0.0
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ ch25-gomodules-version/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cnych/stardust/stringsx"
+	stringsV2 "github.com/cnych/stardust/v2/stringsx"
+)
+
+func main() {
+	fmt.Println(stringsx.Hello("cnych"))
+
+	if greet, err := stringsV2.Hello("cnych", "fr"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(greet)
+	}
+
+}
+
